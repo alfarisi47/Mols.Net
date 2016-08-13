3ce0690d3f0247363d325ffae86ab6af96ac8418
M Salman Al Farisi <MuhammadSalman.AlFarisi@mitrais.com>
2016-08-13 10:16:25 +0700

    - Updated CSS - Added Item Model - Added Category Model - Added Store Controller

---
+++ Mols/Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mols.Models;
+
+namespace Mols.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            var categories = new List<Category>
+            {
+                new Category { CategoryName = "T-Shirt"},
+                new Category { CategoryName = "Jacket"},
+                new Category { CategoryName = "Polo Shirt"}
+            };
+            return View(categories);
+        }
+
+        //
+        // GET: /Store/Browse
+        public ActionResult Browse(string categoryName)
+        {
+            var category = new Category { CategoryName = categoryName };
+            return View(category);
+        }
+
+        //
+        // GET: /Store/Details
+        public ActionResult Details(int id)
+        {
+            var item = new Item { Name = "Item " + id };
+            return View(item);
+        }
+    
+    }
+}
---
+++ Mols/Models/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mols.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockOnHand { get; set; }
+
+        public int StockInOrder { get; set; }
+
+        public string ItemPictureUrl { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        //public virtual List<OrderDetail> OrderDetails { get; set; }
+    }
+}
