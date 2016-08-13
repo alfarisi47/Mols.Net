ed5aaeda5c27a788e15b565d5dbf4c6d3da4a2cf
M Salman Al Farisi <alfarisi@outlook.com>
2016-08-13 07:05:07 +0700

    Initial Commit - Skeleton of .NET MVC - Visual  Studio 2015

---
+++ Mols/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mols.Startup))]
+namespace Mols
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
